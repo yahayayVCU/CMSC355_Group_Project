1e0fe359d48b5452b0c57d25d4ef7e3af8a48a3e
Ayten-Abdelraze <ayten.abdelrazek@gmail.com>
2024-11-01 13:41:42 -0400

    home page

--- Sprint 1/my-app/src/App.js
+++ Sprint 1/my-app/src/App.js
@@ -1,11 +1,55 @@
+import React, { useState } from "react";
 import './App.css';
+import { Link, Routes, Route } from "react-router-dom";
+import FindTherapist from "./findTherapist/therapists";
+import Hobbies from "./hobbies/hobbies";
+import Resources from "./resources/resources";
 
 function App() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, World!</h1>
+        <h1>Welcome to Mental Support</h1>
       </header>
+      
+      {/* Ensure the button container is displayed below the header */}
+      <div className="button-container">
+        <button onClick={togglePopup} className="btn">Resources</button>
+        <button className="btn">Journal</button>
+        <button className="btn">Chat</button>
+      </div>
+
+      {/* Pop-up menu for Resources button */}
+      {showPopup && (
+        <div className="popup">
+          <h3>Resources</h3>
+          <ul>
+            <li>
+              <Link to="/findTherapist" onClick={togglePopup}>Find Therapist</Link>
+            </li>
+            <li>
+              <Link to="/hobbies" onClick={togglePopup}>Hobbies</Link>
+            </li>
+            <li>
+              <Link to="/resources" onClick={togglePopup}>Resources</Link>
+            </li>
+          </ul>
+          <button onClick={togglePopup} className="close-popup">Close</button>
+        </div>
+      )}
+
+      {/* Routes for each linked page */}
+      <Routes>
+        <Route path="/findTherapist" element={<FindTherapist />} />
+        <Route path="/hobbies" element={<Hobbies />} />
+        <Route path="/resources" element={<Resources />} />
+      </Routes>
     </div>
   );
 }
